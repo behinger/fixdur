189e43acfdd9f04e25c96e00e8db547afb426d16
jschepers <jschepers@uni-osnabrueck.de>
2016-10-07 15:44:35 +0200

    Add files via upload

--- experiment/experiment2/whole_image.py
+++ experiment/experiment2/whole_image.py
@@ -10,6 +10,7 @@ from psychopy import visual
 import tools
 import matplotlib.image as mpimg
 import matplotlib.pyplot as plt
+from PIL import ImageDraw
 
 
 path_to_fixdur_files, path_to_fixdur_code = tools.paths()
@@ -33,29 +34,38 @@ else:
    y0 = center[1]
     
 gaussian = np.exp(-4*np.log(2) * ((x-x0)**2 + (y-y0)**2) / fwhm**2)
-gaussian = gaussian*2-1
+#gaussian = gaussian * 255
+gaussian = -(gaussian*2-1)
+
 
 #imgplot = plt.imshow(gaussian)
 
 #mask_im = Image.new('RGB', (1280,960), 'red')
 #mask_im = visual.Circle(surf, units='pix', radius=100)
-mask_im = np.ones((1280, 960))
-mask_im[10:50] = -1
+
+mask_im = np.ones((1080,1920))
+#mask_im = np.ones((960,1280))
+#mask_im[380:560,550:730] = gaussian
+mask_im[450:630,870:1050] = gaussian
+#imgplot = plt.imshow(mask_im)
 
 
 original = Image.open(path_to_fixdur_files+'stimuli/natural/image_5.bmp')
 #original = original.resize((1920,1080))
-black_white = original.convert('L')
+
+
+imgplot = plt.imshow(mask_im)
 
 # Source: https://opensource.com/life/15/2/resize-images-python
 basewidth = 1920
 wpercent = (basewidth / float(original.size[0]))
 hsize = int((float(original.size[1]) * float(wpercent)))
 #original = original.resize((basewidth, hsize), Image.ANTIALIAS)
-imgplot = plt.imshow(original)
+black_white = original.convert('L')
+# imgplot = plt.imshow(original)
 
 
-original_im = visual.ImageStim(surf, image=black_white, mask=-gaussian)
+original_im = visual.ImageStim(surf, image=black_white, mask=mask_im, units='pix')
 #original_im = visual.ImageStim(surf, image=path_to_fixdur_files+'stimuli/natural/image_5.bmp', mask=-gaussian)
 #grating = visual.GratingStim(win=surf, mask='circle', size=300, pos=[-4,0], sf=3)
 
